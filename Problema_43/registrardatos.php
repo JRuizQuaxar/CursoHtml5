9830f82146095a638be35f671e1cb44f3c3a97f4
Juan Alberto <jruiz@quaxar.com>
2021-09-07 19:03:14 -0500

    Problema 43-45 res

---
+++ Problema_43/registrardatos.php
@@ -0,0 +1,10 @@
+<?php
+$nombre = $_POST['nombre'];
+$edad=$_POST["edad"];
+$dni=$_POST["dni"];
+
+echo "<h2>---FORMULARIO RESPUESTAS---</h2>";
+echo "Nombre: " . $nombre . "<br/>";
+echo "Edad: " . $edad . "<br/>";
+echo "DNI: " . $dni . "<br/>";
+?>
---
+++ Problema_44/registrardatos.php
@@ -0,0 +1,10 @@
+<?php
+$valor1 = $_POST['valor1'];
+$valor2 = $_POST['valor2'];
+$contra=$_POST["contra"];
+
+echo "<h2>---FORMULARIO RESPUESTAS---</h2>";
+echo "Valor1: " . $valor1 . "<br/>";
+echo "Valor2: " . $valor2 . "<br/>";
+echo "Contraseña: " . $contra . "<br/>";
+?>
